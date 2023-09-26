9a1704b43edcda568fa725359f2b3750cb0d3c05
Asish Racha <asishracha@microsoft.com>
2023-09-26 13:10:01 -0400

    test GHA

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "836b8cd3-0110-4622-a835-5571ddc176e5"
-  client_id       = "a1c900d9-3ee6-4331-80bb-46df32fa6c2c"
-  client_secret   = "Ldu8Q~mdINgrLp7pOqv6GEjzgY9ftD70jYw0KaBL"
-  tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+  # subscription_id = "836b8cd3-0110-4622-a835-5571ddc176e5"
+  # client_id       = "a1c900d9-3ee6-4331-80bb-46df32fa6c2c"
+  # client_secret   = ""
+  # tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
 }
 
 resource "azurerm_resource_group" "resource_group" {
