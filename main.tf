62b23cd0deed23788e44256bc49b9b56a1762dc3
Asish Racha <asishracha@microsoft.com>
2023-09-26 12:44:00 -0400

    test GHA

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
   required_providers {
     azurerm = {
@@ -8,6 +5,15 @@ terraform {
       version = ">= 3.28.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = "d674c517-8ec5-4b7d-b4fd-71f62f72f0fb"
+  client_id       = "9c699268-0e50-4f69-9703-cd7782b11534"
+  client_secret   = "BwF8Q~R6mk5eL-dy7pDQFf~rIWKxTVQau9ieDa-W"
+  tenant_id       = "dc411c63-1f52-4491-bb51-c4d215a1de23"
 }
 
 resource "azurerm_resource_group" "resource_group" {
