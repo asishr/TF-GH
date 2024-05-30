a12b6c8dca6f76c85c5c300a073d16c61da3cbf1
Asish Racha <asishracha@microsoft.com>
2024-05-30 14:29:45 -0400

    updated yml

--- examples/resource_group/example_resource_group.tf
+++ examples/resource_group/example_resource_group.tf
@@ -1,10 +1,6 @@
-provider "azurerm" {
-  features {}
-}
-
 module "resource_group" {
   source   = "../../modules/common/resource_group"
-  name     = "tf-gh-sample-rg"
+  name     = "tf-gh-sample-rg1"
   location = "Canada Central"
   tags     = { "CostCenter" = "1234" }
 }
