b0db454331f1292b6155aa29c1f21c0c4057e643
Asish Racha <asishracha@microsoft.com>
2024-05-30 09:46:09 -0400

    backend added

--- examples/subscriptions/create-subscription/variables.tf
+++
@@ -1,9 +0,0 @@
-variable billing_account_name {}
-variable enrollment_account_name {}
-variable sub_name {}
-variable workload {}
-variable alias {}
-variable billing_profile_name {}
-variable invoice_section_name {}
-variable tags {}
-
