f9b27c0f981257a436bcc430ae7c96b2d82bd637
Asish Racha <asishracha@microsoft.com>
2024-05-31 09:14:19 -0400

    rem main

--- main.tf
+++ main.tf
@@ -11,19 +11,19 @@ terraform {
     key                   = "dev.tfstate"
     use_oidc             = true # To use OIDC to authenticate to the backend
     client_id            = "293d13af-61f2-415a-a959-dda720544372" # The client ID of the Managed Identity
-    subscription_id      = "d674c517-8ec5-4b7d-b4fd-71f62f72f0fb" # The subscription ID where the storage account exists
-    tenant_id            = "dc411c63-1f52-4491-bb51-c4d215a1de23" # The tenant ID where the subscription and the Managed Identity are
+    subscription_id      = "633c41b2-9e22-4a9c-90fb-299daded47bc" # The subscription ID where the storage account exists
+    tenant_id            = "36f13337-06d5-47b6-80f8-7b85cc344f98" # The tenant ID where the subscription and the Managed Identity are
   }
 }
 
 provider "azurerm" {
   features {}
   use_oidc        = true # Use OIDC to authenticate to Azure
-  subscription_id = "81d07db3-86e1-49db-90a8-994e8228c86d"
+  subscription_id = "633c41b2-9e22-4a9c-90fb-299daded47bc"
 }
 
 module "resource_group" {
-  source   = "../../modules/common/resource_group"
+  source   = "./modules/common/resource_group"
   name     = "tf-gh-sample-rg2"
   location = "Canada Central"
   tags     = { "CostCenter" = "1234" }
