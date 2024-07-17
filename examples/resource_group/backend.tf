d2a7b66874bed2830d415e1bd2b45dd6344348b2
Asish Racha <asishracha@microsoft.com>
2024-07-17 14:26:14 -0400

    acr added

--- examples/resource_group/backend.tf
+++ examples/resource_group/backend.tf
@@ -20,6 +20,8 @@ terraform {
     container_name        = "tfstate"
     key                   = "dev.tfstate"
     use_oidc              = true # To use OIDC to authenticate to the backend
+    tenant_id = "36f13337-06d5-47b6-80f8-7b85cc344f98"
+    client_id = "96844b22-b284-4ab9-b112-6d340b9db420"
   }
 }
 
@@ -27,6 +29,6 @@ provider "azurerm" {
   features {}
   use_oidc        = true # Use OIDC to authenticate to Azure
   subscription_id = "633c41b2-9e22-4a9c-90fb-299daded47bc"
-  tenant_id = "36f13337-06d5-47b6-80f8-7b85cc344f98"
-  client_id = "96844b22-b284-4ab9-b112-6d340b9db420"
+  # tenant_id = "36f13337-06d5-47b6-80f8-7b85cc344f98"
+  # client_id = "96844b22-b284-4ab9-b112-6d340b9db420"
 }
